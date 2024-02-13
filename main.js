ee0012cbe58cd202e66801a3bfab7c40952f3bee
bentobianc <136643842+bentobianc@users.noreply.github.com>
2024-02-13 13:41:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var fullname = firstName.concat(LastName);
+console.log(fullname);
+var multiple = a*b;
+console.log(multiple);
+var sub = a-b;
+console.log(sub);
+var add = a+b;
+console.log(add);
